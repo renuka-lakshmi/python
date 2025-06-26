5314f7181737702c1a87dcd577321e1f03c092d0
renuka-lakshmi <renusai2005@gmail.com>
2025-06-26 14:23:33 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,134 @@
+# a = 123
+# print(a)
+# print(type(a))
+# print(id(a))
+
+# a (variable)
+# a = variable intillization
+# a = 91 variable decalaration
+#variable =  varaiable is a container which should store some information#
+
+# a1 = 89
+# print(a1)
+# print(type(a1))
+# print(id(a1))
+#''' 
+# type()--which tells about the type of a value that internally compiler holding on
+# id()--which tells about adress location of a value
+# '''
+# '''
+# float--which holds all the decimals or fractional
+# '''
+# b = 52.25
+# print(b)
+# print(type(b))
+# print(id(b))
+
+
+
+# # '''
+# # complex datatypes are used to hold the real and imaginary values
+# # which can be represented as a+bj
+# # '''
+# # c = 76+65j
+# # print(c)
+# # print(type(c))
+# # print(id(c))
+
+# # c1 = -90+54j
+# # print(c1)
+# # print(type(c1))
+# # print(id(c1))
+
+
+# # """
+# # sequence datatypes are divided into '3' types
+# # 1.string
+# # 2.list
+# # 3.tuple
+
+# # string  ; is nothing but group of /collection of characters
+# # it can be enclosed by using single quotes are double quotes
+
+# # """
+# h='ists'
+# print(h)
+# print(type(h))
+# print(id(h))
+
+
+# # """
+# # list : list is a collection of elements/items/values
+# # syntax : listname[val1,val2,...valn]
+# # list : is mutable (internal values can change automatically)
+# # list : it can  be denoted as square brackets
+# # """
+
+#mylist = [256,25.69,"hello",52+98j]
+#print(mylist)
+#print(type(mylist))
+#print(id(mylist))
+
+
+"""
+tuple : tuple is a collection of elements/items/values
+syntax : tuplename=( val1,val2,val3...valn)
+tuple : is imutable(internal values cant change)
+tuple : it can be denoted as open parenthesis
+"""
+#mytuple=(78,98+85j,'hi nanna',[2,5,65],(45,55))
+#print(mytuple)
+#print(type(mytuple))
+#print(id(mytuple))
+
+"""
+in order to access the elements individually we use indexing
+indexing always start with zero"0" and ends with n-1
+syntax to access the elements
+print(listname[elementposition])
+"""
+
+
+# mylist = [256,25.69,"hello",52+98j]
+# print(mylist)
+# print(type(mylist))
+# print(id(mylist))
+# print(mylist[2])
+
+# mytuple=(78,98+85j,'hi nanna',[2,5,65],(45,55))
+# print(mytuple)
+# print(type(mytuple))
+# print(id(mytuple))
+# print(mytuple[4])
+
+"""
+boolean : boolean means check the condition
+they are divided into two ways 1.True 2.False
+it can be represented as bool
+"""
+# a = True
+# print(a)
+# print(type(a))
+
+# b = False
+# print(b)
+# print(type(b))
+
+
+"""
+set : it is a collection of values/items/elements
+syntax : setname={val1,val2,...valn}
+a set cannot accept the list..
+"""
+#myset={52,45.69,"renu",("love",456),True}
+#print(myset)
+
+
+# """
+# Dictionary : it is a collection of  keyvalue pairs 
+# syntax : dictionaryname={key1:val1,key2:val2,....keyn: valn}
+
+# """
+mydict={1:"ists", "branch":"ece", "location":"vemagiri"}
+print(mydict)
+print(type(mydict))
