a236554b8c1e96b0e16b4b48b85e8c491d891125
renuka-lakshmi <renusai2005@gmail.com>
2025-07-03 15:03:36 +0530

    Add files via upload

---
+++ comments.py
@@ -0,0 +1,9 @@
+"""
+a comments gives the additional information about the code..
+in python we could see "2" Types of comments
+1. Single line comments-- which can be represented using the #-- generally these are used to write a single line comments
+2. Multiline comments-- which can be represented using the double triple quotes("""""")
+and generally used to write a paragraph or text 
+3.Comments are igonred by compiler
+""" 
+print("hi")#this is a hi statement
